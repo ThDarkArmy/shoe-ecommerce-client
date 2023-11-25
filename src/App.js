5c1a09dc0e399fd8898426619030c3a77e0f12ae
Prashant <thepenseur1729@gmail.com>
2023-11-25 21:40:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes, BrowserRouter as Router}  from 'react-router-dom'
 
-function App() {
+import ForgetPassword from "./pages/ForgetPassword";
+import LoginRegister from "./pages/LoginRegister"
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import SneakerPage from "./pages/SneakerPage";
+import MyCart from "./pages/MyCart";
+import MyOrders from "./pages/MyOrders";
+import AdminPanel from "./pages/AdminPanel";
+import VendorPanel from "./pages/VendorPanel";
+import ProfilePage from "./pages/ProfilePage";
+
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login-register" element={<LoginRegister/>}/>
+        <Route path="/reset-password" element={<ForgetPassword/>}/>
+        <Route path="/sneaker/:id" element={<SneakerPage/>}/>
+        <Route path="/my-cart" element={<MyCart/>}/>
+        <Route path="/my-orders" element={<MyOrders/>}/>
+        <Route path="/admin-panel" element={<AdminPanel/>}/>
+        <Route path="/vendor-panel" element={<VendorPanel/>}/>
+        <Route path="/profile" element={<ProfilePage />}/>
+      </Routes>
+    </Router>
+    <Footer/>
+    </React.Fragment>
   );
 }
 
